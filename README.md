249fe36753f062271696e8ec42c0ff91b0124e5e
Michal Dolnak <dolnakmichal@gmail.com>
2017-10-29 00:35:50 +0200

    Created a simple app with mapbox.

--- README.md
+++ README.md
@@ -18,3 +18,21 @@ password: dbs
 DB dbs
 
 Import data by ogr2ogr from data.json to database dbs.
+
+SSRID UPDATED TO 102067 from ArcGeo. Imported dataset from their site.
+
+Remove angular and add simple index.html and migrate from angular to mapbox!!!
+
+
+SELECT ST_TRANSFORM(wkb_geometry, '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs ', 3857) FROM DATA
+
+SELECT ST_AsText(ST_TRANSFORM(wkb_geometry, '+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs ', 4326)) FROM DATA
+
+Posledna verzia s posunutim 15 metrov juzne a 50 vychodne.
+let all_population_query = 'SELECT \'Feature\' as type, poc_tp, n_u, co, cs,' +
+  'ST_AsGeoJSON(ST_TRANSFORM(wkb_geometry, \'+proj=krovak +lat_0=49.5 +lon_0=24.83333333333333 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs \', 4326),20) as geom FROM DATA limit 50';
+
+Pomenena verzia
+let all_population_query = 'SELECT \'Feature\' as type, poc_tp, n_u, co, cs,' +
+  'ST_AsGeoJSON(ST_TRANSFORM(wkb_geometry, \'+proj=krovak +lat_0=49.689 +lon_0=24.8315 +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs \', 4326),20) as geom FROM DATA limit 50';
+
--- backend/app.js
+++ backend/app.js
@@ -1,45 +1,31 @@
-// call the packages we need
-let express    = require('express');        // call express
-let app        = express();                 // define our app using express
+let express    = require('express');
+let app        = express();
 let bodyParser = require('body-parser');
 let pg = require('pg');
 
-let death = require('death'); // Gonna catch them all!!!
 
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-let port = process.env.PORT || 8083;        // set our port
-
-
-
-// START THE SERVER
-// =============================================================================
+let port = process.env.PORT || 8085;        // set  port
 
 let client = new pg.Client({
   user: 'dbs',
   host: 'localhost',
   database: 'dbs',
   password: 'dbs',
-  port: 5432
+  port: 5433
 });
 
 (async function () {
   await client.connect();
 })();
 
-death(async function(signal, err) {
-  //clean up code here
-  console.log('Death!!!');
-  await client.end();
-});
-
-let router = express.Router(); // get an instance of the express Router
-
-// test route to make sure everything is working (accessed at GET http://localhost:8083/api)
-router.get('/', async function(req, res) {
+let router = express.Router();
+// api calls
+router.get('/api/test', async function(req, res) {
   try {
     let func = async function () {
       return await client.query('SELECT NOW()');
@@ -47,18 +33,59 @@ router.get('/', async function(req, res) {
     let result = await func();
     res.json({message: result});
   } catch (err) {
-    res.error(500);
+    console.log(err);
+    res.res.sendStatus(500);
   }
 });
 
-// ROUTES FOR OUR API
-// =============================================================================
+router.get('/api/population', async function(req, res) {
+  try {
+    let data = await client.query(all_population_query);
 
-// more routes for our API will happen here
+   // console.log(data.rows);
+    data.rows.forEach(function (row) {
+      row.geometry = JSON.parse(row.geom);
+      row.properties = {
+        "title": row.cs + '/'+ row.co  + ' - ' + row.poc_tp + ' obyv.',
+        "icon":"marker"
+      };
+    });
+    res.json(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.res.sendStatus(500);
+  }
+});
+let all_population_query = 'SELECT \'Feature\' as type, poc_tp, n_u, co, cs,' +
+  'ST_AsGeoJSON(ST_TRANSFORM(wkb_geometry, \'+proj=krovak +lat_0=49.689 +lon_0=24.8315 ' +
+  '+alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs \', 4326),20) as geom FROM DATA';
 
-// REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+router.get('/api/streets', async function(req, res) {
+  try {
+    let data = await client.query(grouped_streets);
 
+    console.log(data.rows);
+    data.rows.forEach(function (row) {
+      row.geometry = JSON.parse(row.geom);
+      row.properties = {
+        "title": row.title + ' - '+ row.sum + ' obyv.',
+      };
+    });
+    res.json(data.rows);
+  } catch (err) {
+    console.log(err);
+    res.res.sendStatus(500);
+  }
+});
+let grouped_streets = 'SELECT SUM(poc_tp),n_u as title,ST_AsGeoJSON(ST_MakeLine(ST_TRANSFORM(wkb_geometry, \'+proj=krovak +lat_0=49.689 +lon_0=24.8315 ' +
+  ' +alpha=30.28813975277778 +k=0.9999 +x_0=0 +y_0=0 +ellps=bessel +units=m +no_defs \', 4326)), 20) as geom FROM DATA  GROUP BY n_u';
+
+// inicialization
+router.get('/', async function(req, res) {
+  res.sendFile(('/home/michal/Desktop/advanced-dbs/backend/index.html'));
+});
+
+
+app.use(router)
 app.listen(port);
 console.log('Magic happens on port ' + port);
